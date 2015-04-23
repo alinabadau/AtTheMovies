fb450c593c74efff18ac0cc68f7640c759cda1fd
alinabadau <alina.badau@yahoo.com>
2015-04-23 23:27:12 +0300

    Initial Dump

---
+++ AtTheMovies/Client/Scripts/atTheMovies.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    var app = angular.module("atTheMovies", ["ngRoute"]);
+    var config = function ($routeProvider) {
+        $routeProvider
+        .when("/list",
+        { templateUrl: "/Client/Views/list.html" })
+        .when("/details/:id",
+        { templateUrl: "/Client/Views/details.html" })
+        .otherwise(
+        { redirectTo: "/list" });
+    };
+    config.$inject = ["$routeProvider"];
+
+    app.config(config);
+    app.constant("movieApiUrl", "/api/movies/");
+}());
